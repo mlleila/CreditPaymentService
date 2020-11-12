24cdbb78ad2535008f0e619d1120194b26483609
Leila Mamedova <m.lleila@bk.ru>
2020-11-12 19:11:12 +0300

    Class modified

--- CreditPaymentService.java
+++ CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public long calculate(int sum, int months) {
-        double interest = ((9.99/months) * 0.01) ; //  Месячная процентная ставка
+        double interest = 9.99/100/12 ; //  Месячная процентная ставка
         int payment = months; //количество платежей = количеству месяцев
         double annuity = (interest * Math.pow((1+interest),payment)) / (Math.pow((1+interest),payment) - 1); // коэффициент аннуитета
         int mainPayment = (int)(sum * annuity); // аннуитетный платеж
